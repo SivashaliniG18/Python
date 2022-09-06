50c32274cfd1731da89d7ea90091d3808ce76e56
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-09-06 22:50:55 +0530

    Add files via upload

---
+++ big.py
@@ -0,0 +1,9 @@
+a=int(input("enter first num"))
+b=int(input("enter first num"))
+c=int(input("enter first num"))
+if(a>b) and (a>c):
+    print("A is big")
+elif(b>a) and(b>c):
+    print("B is big")  
+else:
+    print("C is big")     
